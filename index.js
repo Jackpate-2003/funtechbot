f7c3a19d537884eb8073c2ccbed645915ab287cb
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2022-12-25 23:52:46 +0330

    2 commit !

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const express = require('express');
-const { Telegraf } = require("telegraf");
+const {Telegraf} = require("telegraf");
+const BodyParser = require("body-parser");
 
 const BOT_KEY = '5836436547:AAE_Z-6MpCP-bVp3r96M8XhFIMCGxNJgKvk';
-const secret = '/' + 'Rgrthdsjfdvbf31rc';
 
 const HOST = 'https://beige-seal-wear.cyclic.app';
 
 const bot = new Telegraf(BOT_KEY);
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 
-app.use(bot.webhookCallback(`${secret}`));
+const secret = `/tel/${bot.secretPathComponent()}`;
 
-bot.command("cat", ctx => ctx.reply('Hello!'));
+bot.telegram.setWebhook(`${HOST}/${secret}`)
+    .then((status) => console.log('Webhook setted: ' + status))
+    .catch(err => console.log("ERR", err));
 
-bot.telegram.setWebhook(`${HOST}/${secret}`);
+console.log('Secret path', `${HOST}/${secret}`)
+
+bot.command("text", async (ctx) => {
+
+    console.log("Reply!");
+
+    ctx.reply('Hello!')
+});
+
+app.use(BodyParser.json());
+app.use(
+    BodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/", (req, res) => res.send("Hello!"));
+
+app.use(bot.webhookCallback(secret));
 
 app.listen(process.env.PORT || 3000, () => {
 
     console.log('I\'m ready!');
 
 });
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
