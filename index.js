f763c19bc2ba92fa5f8f2d0ed519ba2ad49ab32b
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2022-12-25 23:12:35 +0330

    First commit !

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express');
 const { Telegraf } = require("telegraf");
 
 const BOT_KEY = '5836436547:AAE_Z-6MpCP-bVp3r96M8XhFIMCGxNJgKvk';
+const secret = '/' + 'Rgrthdsjfdvbf31rc';
+
+const HOST = 'https://beige-seal-wear.cyclic.app';
 
 const bot = new Telegraf(BOT_KEY);
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+app.use(bot.webhookCallback(`${secret}`));
 
 bot.command("cat", ctx => ctx.reply('Hello!'));
 
-bot.launch();
+bot.telegram.setWebhook(`${HOST}/${secret}`);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+
+    console.log('I\'m ready!');
+
+});
