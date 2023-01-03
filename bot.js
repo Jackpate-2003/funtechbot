9fdbb9a61b13c6740a431617a176af74f8e02d83
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2023-01-04 01:27:01 +0330

    2 commit !

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const {REG} = require("./utils");
-const {downloadFromYoutube, downloadFromSoundcloud, downloadFromInstagram, downloadFromTiktok, downloadFromFacebook,
+const {
+    downloadFromYoutube, downloadFromSoundcloud, downloadFromInstagram, downloadFromTiktok, downloadFromFacebook,
     downloadFromTwitter
 } = require("./api/download");
 const {youtubeInfo} = require("./api/downloader");
@@ -78,11 +79,66 @@ function start(bot) {
 
     });
 
+    const DOWNLOADER_MSG = 'To download from Youtube, Instagram, TikTok, Twitter, Facebook, Pinterest and Soundcloud, just enter the link of the content you want to download!';
+
+    // DESC HEARS
+    bot.hears('Downloader', async (ctx) => {
+
+        await ctx.reply(DOWNLOADER_MSG);
+
+    });
+
+    bot.start(async (ctx) => {
+
+        await ctx.reply(
+            "<b>Welcome to FunTech Bot!</b>\n We have a lot of tools and we are going to add a lot of other tools! To start, you can see the list of commands and their descriptions with the /help command, or use the menus below."
+            , {
+                "reply_markup": {
+                    "resize_keyboard": true,
+                    "keyboard": [
+                        ["Downloader"],
+                    ]
+                }, parse_mode: 'HTML',
+            })
+    });
+
+    // Commands
+    bot.command('help', async (ctx) => {
+
+        await ctx.reply(`
+    ${DOWNLOADER_MSG}
+    <b>/help</b>  <i>List of commands</i>
+    <b>/funtech</b>  <i>About us</i>
+    `, {
+            parse_mode: 'HTML',
+            reply_to_message_id: ctx.message.message_id,
+        })
+
+    });
+
+    bot.command('funtech', async (ctx) => {
+
+        await ctx.reply(`
+        FunTech is a practical collection of various entertainment, scientific and IT tools.\n
+        <a href="https://www.youtube.com/@fun--tech">Youtube channel: @fun--tech</a> 
+        <a href="https://t.me/+qGGw48DBe000NDc0">Telegram channel: Fun Tech!</a>
+        `, {
+            parse_mode: 'HTML',
+            reply_to_message_id: ctx.message.message_id,
+        });
+
+    });
+
     bot.action('download_youtube', async (ctx) => {
 
         return await downloadFromYoutube(ctx);
 
     });
+
+    bot.telegram.setMyCommands([
+        {command: "help", description: "List of commands"},
+        {command: "funtech", description: "About & contact us"},
+    ]);
 
 }
 
