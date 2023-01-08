fad715f99b2a022063c66d0f66da0df0f6ddc8b4
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2023-01-09 03:07:49 +0330

    2 commit !

--- bot.js
+++ bot.js
@@ -146,7 +146,7 @@ function start(bot) {
 
     });
 
-    bot.hears(/(music) (.*)/i, async (ctx) => {
+    bot.hears(/(music) (.*)/, async (ctx) => {
 
         return await waitForSent(ctx, async (ctx) => {
 
