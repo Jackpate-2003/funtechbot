84e08324170430b85d4304faab4369de37915bee
Jackpate-2003 <63040769+Jackpate-2003@users.noreply.github.com>
2023-01-08 19:23:08 +0330

    2 commit !

--- utils/youtube-music/index.js
+++ utils/youtube-music/index.js
@@ -20,18 +20,18 @@ async function downloadResults(ids, tracksCount = 1) {
 
         const audio = ytdl(ids[i], {filter: 'audioonly', quality: 'highestaudio'});
 
-        const ffmpegProcess = cp.spawn(ffmpegStatic, [
+    /*    const ffmpegProcess = cp.spawn(ffmpegStatic, [
             '-loglevel', '0', '-hide_banner',
             '-i', 'pipe:0',
             '-f', 'mp3',
             'pipe:1'
         ]);
 
-        audio.pipe(ffmpegProcess.stdio[0]);
+        audio.pipe(ffmpegProcess.stdio[0]);*/
 
         resultArray.push(
             await new Promise((res, rej) => {
-                ffmpegProcess.stdout.on('data', chunk => {
+                audio.on('data', chunk => {
                     chunks.push(chunk);
                 }).on('end', () => {
                     res(Buffer.concat(chunks));
